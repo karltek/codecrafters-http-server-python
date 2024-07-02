d93c725f9e292793f813a352470b31c08a3290fa
karltek <elrakeroz@gmail.com>
2024-07-02 15:05:00 +0800

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,16 +1,31 @@
-# Uncomment this to pass the first stage
 import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
+    print("Waiting for connection")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    # server_socket.accept() # wait for client
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    
+    # server_socket.accept() returns a Tuple so we store them in two variables, connection & address
+    connection, address = server_socket.accept()
+    # print( addr[0])
+    # server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    
+    while True:
+        data = connection.recv(1024)
+        # print("Print decoded data.")
+
+        result = data.decode().split("\r\n")
+        
+        res_200 = b"HTTP/1.1 200 OK\r\n\r\n"
+        res_400 = b"HTTP/1.1 404 Not Found\r\n\r\n"
+        if result[0].split(" ")[1] == "/":
+            connection.sendall(res_200)
+        else:
+            connection.sendall(res_200)
+        
+        connection.close()
+
+
 
 if __name__ == "__main__":
     main()
